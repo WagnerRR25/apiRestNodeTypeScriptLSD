1609238db397681c8a3f0d9f6a27f5c37374ca71
WagnerRR25 <rodrigueswagner09@gmail.com>
2023-09-02 02:28:27 -0300

    feat: Generate JWT

--- src/server/controllers/usuarios/SignIn.ts
+++ src/server/controllers/usuarios/SignIn.ts
@@ -2,10 +2,10 @@ import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import * as yup from 'yup';
 
-import { UsuariosProvider } from '../../database/providers/usuarios';
-import { validation } from '../../shared/middleware';
 import { IUsuario } from '../../database/models';
-import { PasswordCrypto } from '../../shared/services';
+import { validation } from '../../shared/middleware';
+import { JWTService, PasswordCrypto } from '../../shared/services';
+import { UsuariosProvider } from '../../database/providers/usuarios';
 
 
 interface IBodyProps extends Omit<IUsuario, 'id' | 'nome'> { }
@@ -20,8 +20,8 @@ export const signInValidation = validation((getSchema) => ({
 export const signIn = async (req: Request<{}, {}, IBodyProps>, res: Response) => {
   const { email, senha } = req.body;
 
-  const result = await UsuariosProvider.getByEmail(email);
-  if (result instanceof Error) {
+  const usuario = await UsuariosProvider.getByEmail(email);
+  if (usuario instanceof Error) {
     return res.status(StatusCodes.UNAUTHORIZED).json({
       errors: {
         default: 'Email ou senha são inválidos'
@@ -29,16 +29,24 @@ export const signIn = async (req: Request<{}, {}, IBodyProps>, res: Response) =>
     });
   }
 
-  const paswordMatch = await PasswordCrypto.verifyPassword(senha, result.senha);
+  const paswordMatch = await PasswordCrypto.verifyPassword(senha, usuario.senha);
   if (!paswordMatch) {
     return res.status(StatusCodes.UNAUTHORIZED).json({
       errors: {
-        default: 'Email ou senha são inválidos'
+        default: 'Email ou senha são inválidos!'
       }
     });
   } else {
-    return res.status(StatusCodes.OK).json({ accessToken: 'teste.teste.teste' })
+    
+    const accessToken = JWTService.sign({ uid: usuario.id });
+    if (accessToken === 'JWT_SECRET_NOT_FOUND') {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        errors: {
+          default: 'Erro ao gerar o Token de acesso!'
+        }
+      });
+    }
+    
+    return res.status(StatusCodes.OK).json({ accessToken })
   }
-
-  return res.status(StatusCodes.CREATED).json(result);
 };
